9254a0a7162f41e0a5519efc9d8beef16e27186c
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 10:46:28 +0200

    tired

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return app.send_static_file('home.html')
     
 if __name__ == "__main__":
     app.run()
 
 @app.route('/upcoming')
-def index():
+def upcoming():
     return app.send_static_file('upcoming.html')
     
 if __name__ == "__main__":
