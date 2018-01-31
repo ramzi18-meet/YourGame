28f417c7e156c09888ce19563c0d6347c026716f
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 11:05:37 +0200

    upcoming

--- app.py
+++ app.py
@@ -14,3 +14,18 @@ def upcoming():
     
 if __name__ == "__main__":
     app.run()
+
+@app.route('/upcoming')
+def contact():
+    return app.send_static_file('contact.html')
+    
+if __name__ == "__main__":
+    app.run()
+
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+    
+if __name__ == "__main__":
+    app.run()
