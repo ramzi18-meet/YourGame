c58c87d9ab9574b0d060d1a07e827aa1d1332688
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 15:11:35 +0200

    working

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+
+
+
+@app.route('/')
 @app.route('/home')
 def home():
-    return app.send_static_file('home.html')
+    return render_template('home.html')
     
-if __name__ == "__main__":
-    app.run()
+
 
 
 
@@ -14,10 +20,9 @@ if __name__ == "__main__":
 
 @app.route('/upcoming')
 def upcoming():
-    return app.send_static_file('upcoming.html')
+    return render_template('upcoming.html')
     
-if __name__ == "__main__":
-    app.run()
+
 
 
 
@@ -26,17 +31,43 @@ if __name__ == "__main__":
 
 @app.route('/contact')
 def contact():
-    return app.send_static_file('contact.html')
+    return render_template('contact.html')
     
-if __name__ == "__main__":
-    app.run()
+
+
 
 
 
 
 @app.route('/about')
 def about():
-    return app.send_static_file('about.html')
-    
+    return render_template('about.html')
+  
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    name = request.form['Name']
+    email = request.form['Email']
+    password = request.form['Password']
+    save = sign(name, email, password)
+    db.session.add(save)
+    db.session.commit()
+    users = sign.query.all() 
+    return render_template('upcoming.html')       
+
+
+class sign(db.Model):
+    __tablename__ = "sign"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column('Name', db.Unicode)
+    email = db.Column('Email', db.Unicode)
+    password = db.Column('Password', db.Unicode)
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+db.create_all()
+
+
+
 if __name__ == "__main__":
     app.run()
