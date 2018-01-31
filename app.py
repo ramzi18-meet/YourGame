5fd56b5cbf316d40f71fefaf0211f845d4d7a79a
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 09:37:46 +0200

    fixed

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+app = Flask(__name__, static_url_path='')
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('home.html')
+    return app.send_static_file('home.html'/)
 if __name__ == "__main__":
     app.run()
