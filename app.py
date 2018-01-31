88655f7e7b635e06960fad2602460814daad3286
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 10:31:55 +0200

    static

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Yo, it's working!"
-@app.route('/home/')
-def root():
     return app.send_static_file('home.html')
+    
 if __name__ == "__main__":
     app.run()
