8e4a594faed6b2b52d0c3da4db7b4eef6737c5aa
ramzi18-meet <ramzi18@meet.mit.edu>
2018-01-31 10:16:13 +0200

    2rift

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Yo, it's working!"
-@app.route('/index/')
+@app.route('/home/')
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file('home.html')
 if __name__ == "__main__":
     app.run()
